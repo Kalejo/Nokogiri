670a972eacc4fa5c91d75bfa8acb398875bdff48
Kalejo <alejo_alvarez67@hotmail.com>
2018-04-12 22:38:32 +0200

    2eme motif

--- Trader.rb
+++ Trader.rb
@@ -4,21 +4,20 @@ require "nokogiri"
 require "open-uri"
 
 def cryptocurrency
-
+	cryptomonnaies = []
 	doc = Nokogiri::HTML(open('https://coinmarketcap.com/all/views/all/'))
+	doc.css("a.currency-name-container").each_with_index do | l, x | 
+	# avec index : je met en proportionnalité la valeur de la monnaie avec le cours du prix (valeur de référence)
+	# l'élement (l) prend la valeur de nom de monnaies
+	# l'élement (x) prend la valeur du prix
+	puts l.text
+	puts x[data-btc]
 
-	doc.css("a.currency-name-container").each do |l, x| 
-# l'élement (l) prend la valeur de nom de monnaies
-# l'élement (x) prend la valeur 
 
-
-		cryptomonnaies[x] = Hash.new(0)
-		cryptomonnaies[x][l.text] = doc.css("a.price")[x].text
-		puts cryptomonnaies[x]
-
+	cryptomonnaies[x] = Hash.new(0)
+	cryptomonnaies[x][l.text] = doc.css("a.price")[0].text
+	puts cryptomonnaies[x]
+	end
+	p cryptomonnaies
 end
-
-end
-
-
 cryptocurrency
